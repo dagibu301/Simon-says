b273adbf8dbca0689229f442b7f92fb9223fae15
David Giraldo <davidgiraldobueno@gmail.com>
2021-05-08 17:54:11 -0400

    Initialize

--- script.js
+++ script.js
@@ -7,10 +7,25 @@ const btnStart = document.getElementById("btnStart");
 class Game {
   constructor() {
     this.initialize();
+    this.generateSequence();
   }
 
   initialize() {
     btnStart.classList.add("hide");
+    this.level = 1;
+    this.colores = {
+      blue,
+      purple,
+      orange,
+      green,
+    };
+  }
+
+  generateSequence() {
+    this.sequence = new Array(10)
+      .fill(0)
+      .map((n) => Math.floor(Math.random() * 4), 0, 10);
+    console.log(this.sequence);
   }
 }
 
